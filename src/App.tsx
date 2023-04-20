fca6d8030dbeae59a28c6c7bc3a799045b3c5966
liranmeir <liranmeir@gmail.com>
2023-04-20 13:03:09 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,45 @@ import liranImg from "./assets/liran.png";
 import liranAudio from "./assets/liran.mp3";
 import tzippiImg from "./assets/tzippi.png";
 import tzippiAudio from "./assets/tzippi.mp3";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import galImg from "./assets/gal.png";
+import galAudio from "./assets/gal.mp3";
+import benImg from "./assets/ben.png";
+import benAudio from "./assets/ben.mp3";
+import razImg from "./assets/raz.png";
+import razAudio from "./assets/raz.mp3";
 
-const family = ["liran", "tzippi", "gal"]
+import "./App.css";
+type FamilyAudioMap = {
+  [key: string]: {
+    audio: HTMLAudioElement;
+    image: string;
+  };
+};
+const FAMILY_AUDIO_MAP: FamilyAudioMap = {
+  liran: { audio: new Audio(liranAudio), image: liranImg },
+  tzippi: { audio: new Audio(tzippiAudio), image: tzippiImg },
+  gal: { audio: new Audio(galAudio), image: galImg },
+  ben: { audio: new Audio(benAudio), image: benImg },
+  raz: { audio: new Audio(razAudio), image: razImg },
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [audio] = useState(new Audio(liranAudio));
-
-  const handleClick = () => {
+  const handleClick = (p: string) => {
+    const audio = FAMILY_AUDIO_MAP[p].audio;
     audio.play();
   };
 
   return (
     <div className="App">
-      {/* <div>
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-        <img src={liranImg} className="logo" />
-      </div> */}
-      <div className="card">
-        <button onClick={handleClick}>
-          {/* count is {count} */}
-          <img src={liranImg} className="card-image" />
-        </button>
+      <h1 style={{ textAlign: "center" }}>ה מ ש פ ח ה . ש ל י </h1>
+      <div className="wrapper">
+        {Object.keys(FAMILY_AUDIO_MAP).map((p) => (
+          <div className="card">
+            <button onClick={() => handleClick(p)}>
+              <img src={FAMILY_AUDIO_MAP[p].image} className="card-image" />
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
